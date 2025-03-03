e3a8ad8580bbe3ff9d0a755241336a53eeb5fc5e
SureshK2004 <sureshksureshk04gmail.com>
2025-03-03 16:55:50 +0530

    till collection

---
+++ shop/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect,HttpResponse
+from .models import Category,Products
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+    return render(request, "shop/index.html")
+
+def register(request):
+    return render(request, "shop/register.html")
+
+def login(request):
+    return render(request, "shop/login.html")
+
+def collection(request):
+    category = Category.objects.filter(status=0)  # Renamed variable for consistency
+    return render(request, "shop/collection.html", {"category": category})  # Updated context key
+
+def collectionview(request, name):
+    if Category.objects.filter(name=name, status=0).exists():
+        products = Products.objects.filter(category__name=name)  # Fixed the filter
+        return render(request, "shop/products/index.html", {"products": products,"category_name":name})
+    else:
+        messages.warning(request, "No such category found")
+        return redirect("collection")
+
+
+
+def product_details(request, cname, pname):
+    if Category.objects.filter(name=cname, status=0).exists():
+        product = Products.objects.filter(name=pname, status=0).first()
+        if product:
+            return render(request, "shop/products/product_details.html", {"products": product})
+        else:
+            messages.error(request, "No such product found")
+            return redirect("collection")  
+    else:
+        messages.error(request, "No such category found")
+        return redirect("collection")
