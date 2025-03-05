f3e1bc6eb3bdf0a07e1fae9d8d3a7f6d378ae980
SureshK2004 <sureshksureshk04gmail.com>
2025-03-05 22:44:11 +0530

    till register and login not works

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path("",views.home,name="home"),
+    path("/",views.home,name="home"),
     path("/register",views.register,name="register"),
-    path("/login",views.login,name="login"),
+    path('login/', views.login_page, name='login'), 
+    path('logout/', views.logout_page, name='logout'), 
     path("/collections",views.collection,name="collection"),
     path('collections/<str:name>', views.collectionview, name='collection'),
     path('collections/<str:cname>/<str:pname>', views.product_details, name='product_details'),
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,HttpResponse
 from .models import Category,Products
 from django.contrib import messages
 from shop.form import CustomUserForm
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def home(request):
@@ -10,10 +11,59 @@ def home(request):
 
 def register(request):
     form=CustomUserForm()
+    if request.method=='POST':
+        form=CustomUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Registration Success you can login")
+            return redirect('/login')
     return render(request, "shop/register.html",{'form':form})
 
-def login(request):
-    return render(request, "shop/login.html")
+# def login_page(request):
+#     if request.user.is_authenticated:
+#         return redirect("/")
+#     else:
+#         if request.method=='POST':
+#             name=request.POST.get('username')
+#             pwd=request.POST.get('password')
+#             user=authenticate(request,username=name,password=pwd)
+#             if user is not None:
+#                 messages.success(request,"Logged in Succcessfully")
+#                 return redirect('/')
+#             else:
+#                 messages.error(request,"Invalid Username or Password")
+#                 return redirect("/login")
+
+#         return render(request, "shop/login.html")
+
+ 
+ 
+def login_page(request):
+  if request.user.is_authenticated:
+    return redirect("/")
+  else:
+    if request.method=='POST':
+      name=request.POST.get('username')
+      pwd=request.POST.get('password')
+      user=authenticate(request,username=name,password=pwd)
+      if user is not None:
+        login(request,user)
+        messages.success(request,"Logged in Successfully")
+        return redirect("/")
+      else:
+        messages.error(request,"Invalid User Name or Password")
+        return redirect("/login")
+    return render(request,"shop/login.html")
+
+
+
+ 
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logout Success")
+    return redirect("/")
+
 
 def collection(request):
     category = Category.objects.filter(status=0)  # Renamed variable for consistency
