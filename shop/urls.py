851767ba876cce6307ba60e668ca4d783ff4e1b8
SureshK2004 <sureshksureshk04gmail.com>
2025-04-18 18:42:37 +0530

    authentication finish

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path("/",views.home,name="home"),
-    path("/register",views.register,name="register"),
-    path('login/', views.login_page, name='login'), 
-    path('logout/', views.logout_page, name='logout'), 
-    path("/collections",views.collection,name="collection"),
+    path("",views.home,name="home"),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path("collections/",views.collection,name="collection"),
     path('collections/<str:name>', views.collectionview, name='collection'),
-    path('collections/<str:cname>/<str:pname>', views.product_details, name='product_details'),
+    path('collections/<str:cname>/<str:pname>', views.product_details, name='product_details'),  
 ]
 
